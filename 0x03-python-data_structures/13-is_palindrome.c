599a915a87c1287f5be8bb391059c0c738cb18bb
esraa <esraad98d@gmail.com>
2023-12-04 02:59:58 -0800

    commit

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <string.h>
 /**
 *add_nodeint - adds a new node at the beginning of a listint_t list
 *@head: head of listint_t
